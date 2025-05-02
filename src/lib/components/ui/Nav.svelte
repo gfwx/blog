3536557dd88c6679ef34237f081280cc2042b737
SK <gfawkes1832@gmail.com>
2025-05-02 11:12:54 +0530

    project restructure

---
+++ src/lib/components/ui/Nav.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	let active = $state('');
+	const handleClick = (state: string) => {
+		active = state;
+		goto(`/${state}`);
+	};
+</script>
+
+<div class="navbar bg-base-100 shadow-sm">
+	<a class="flex-1" href="/">gfwx</a>
+	<div class="flex-none">
+		<ul class="tabs tabs-border" role="tablist">
+			<li>
+				<button
+					onclick={() => handleClick('')}
+					role="tab"
+					class="tab tab-active"
+					class:tab-active={active === ''}>blog</button
+				>
+			</li>
+			<li>
+				<button
+					onclick={() => handleClick('about')}
+					role="tab"
+					class="tab"
+					class:tab-active={active === 'about'}>about</button
+				>
+			</li>
+		</ul>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let { children } = $props();
+	import '../app.css';
+	import { app } from '$lib/firebase';
+	import { onMount } from 'svelte';
+	import Nav from '$lib/components/ui/Nav.svelte';
+
+	onMount(() => {
+		console.log(app);
+	});
+</script>
+
+<Nav />
+{@render children()}
